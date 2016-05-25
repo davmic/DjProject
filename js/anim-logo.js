67f0b35a0a505beb5c91ad2f351bd8aca76d943d
meeelgd <mel.begyn@gmail.com>
2016-05-25 05:55:26 +0200

    add images animation "Boccara DJ" + Vinyle compil + Platine2 + MAJ  médias du dossier images

---
+++ js/anim-logo.js
@@ -0,0 +1,15 @@
+window.setInterval(function(){
+   window.setTimeout(function(){
+       window.setTimeout(function(){
+                  window.setTimeout(function(){
+                        window.setTimeout(function(){
+                            $("#logo2").css('background-image','url(images/logo-anim1.png)');
+                        },1000);   
+                     $("#logo2").css('background-image','url(images/logo-anim2.png)');
+                  },1000);         
+          $("#logo2").css('background-image','url(images/logo-anim3.png)');
+      },1000);
+      $("#logo2").css('background-image','url(images/logo-anim4.png)');
+   },1000);
+      $("#logo2").css('background-image','url(images/logo-anim5.png)');
+},2000);
